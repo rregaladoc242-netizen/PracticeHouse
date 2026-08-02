74f8d28dc62f3030fec79d537b1653108a986440
MARC <rregaladoc_24_2@unc.edu.pe>
2026-08-02 18:12:43 -0500

    Juego de tragamonedas v1 finalizado

---
+++ SEMANA-07/TragamonedasV1/MainWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TragamonedasV1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timerReloj;
+        private DispatcherTimer timerJuego;
+        private Random random = new Random();
+
+        private const int TIEMPO_TOTAL_TICKS = 60;
+        private int contadorTics = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //CREAR EL RELOJ 
+            timerReloj = new DispatcherTimer();
+            timerReloj.Interval = TimeSpan.FromMilliseconds(1);
+            timerReloj.Tick += TimerReloj_Tick;
+            timerReloj.Start();
+
+            //CREAR EL TEMPORIZADOR DEL JUEGO
+            timerJuego = new DispatcherTimer();
+            timerJuego.Interval = TimeSpan.FromMilliseconds(100);
+            timerJuego.Tick += TimerJuego_Tick;
+            timerJuego.Start();
+        }
+
+        private void btnIniciarJuego_Click(object sender, RoutedEventArgs e)
+        {
+            timerJuego.Start();
+            contadorTics = 0;
+            lblResultado.Visibility = Visibility.Hidden;
+            btnIniciarJuego.IsEnabled = false;
+        }
+
+        private void TimerReloj_Tick(object sender, EventArgs e)
+        {
+            lblReloj.Content = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+        private void TimerJuego_Tick(object sender, EventArgs e)
+        {
+            int n1 = random.Next(10, 31);
+            int n2 = random.Next(10, 31);
+            int n3 = random.Next(10, 31);
+
+            txtJugada1.Text = n1.ToString();
+            txtJugada2.Text = n2.ToString();
+            txtJugada3.Text = n3.ToString();
+
+            contadorTics++;
+            if(contadorTics >= TIEMPO_TOTAL_TICKS)
+            {
+               timerJuego.Stop();
+
+                Validar_Jugada(n1, n2, n3);
+
+            }
+        }
+
+        private void Validar_Jugada(int n1 ,int n2, int n3)
+        {
+            if(n1 == n2 && n2 == n3)
+            {
+                lblResultado.Content = "GANASTE EL PREMIO MAYOR";
+                lblResultado.Background = Brushes.Green;
+            }
+            else
+            {
+                lblResultado.Content = "PERDISTE, INTENTA DE NUEVO";
+                lblResultado.Background = Brushes.Red;
+            }
+
+            lblResultado.Visibility = Visibility.Visible;
+            btnIniciarJuego.IsEnabled = true;
+        }
+    }
+}
