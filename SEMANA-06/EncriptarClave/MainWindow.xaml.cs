fa81d86a969930cb8a6a82d2f1f250d7d8ee43d4
MARC <rregaladoc_24_2@unc.edu.pe>
2026-08-02 14:00:27 -0500

    Ejercicio Encriptar Clave Finalizado

---
+++ SEMANA-06/EncriptarClave/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EncriptarClave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncriptar_Click(object sender, RoutedEventArgs e)
+        {
+            string clave = txtClave.Text;
+            if (string.IsNullOrWhiteSpace(clave))
+            {
+                MessageBox.Show("Por favor, ingrese una clave." , "Clave invalida" , MessageBoxButton.OK , MessageBoxImage.Warning);
+                txtClave.Focus();
+                return;
+            }
+
+            string claveEncriptada = "";
+            int desplazamiento = 3;
+            foreach (char caracter in clave)
+            {
+                char nuevoCaracter = (char)(caracter + desplazamiento);
+                claveEncriptada += nuevoCaracter.ToString();
+            }
+
+            txtClaveEncriptada.Text = claveEncriptada;
+        }
+
+        private void btnDesencriptar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
