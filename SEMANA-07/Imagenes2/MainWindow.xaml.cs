caa9bb921ab15c609e5dc51f7740ecf1ae2e8f1c
MARC <rregaladoc_24_2@unc.edu.pe>
2026-08-06 10:43:25 -0500

    Ejercicio Imagenes2 finalizado

---
+++ SEMANA-07/Imagenes2/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Imagenes2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Cargar la imagen desde un archivo
+            /*
+            BitmapImage bitmapImage = new BitmapImage();
+            bitmapImage.BeginInit();
+            bitmapImage.UriSource = new Uri(@"C:\Users\alexa\DEV\Proyectos\VS Community\PracticeHouse\SEMANA-07\Imagenes2\plaza_armas.jpg");
+            bitmapImage.EndInit();
+            imagen1.Source = bitmapImage;
+            */
+
+            imagen1.Source = new BitmapImage(new Uri(@"C:\Users\alexa\DEV\Proyectos\VS Community\PracticeHouse\SEMANA-07\Imagenes2\plaza_armas.jpg"));
+            imagen2.Source = new BitmapImage(new Uri("pack://application:,,,/Imagenes/Google.png"));
+        }
+    }
+}
